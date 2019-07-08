2053de0eecfde4358cca9873793d0d628cb14cd8
bpotteiger1 <bpottei1@umbc.edu>
2019-07-08 16:27:31 -0700

    Commit two

--- twincompile.sh
+++ twincompile.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-clang++ -flto -fuse-ld=gold -frandom-seed=42 -g -O0 -Wl,--plugin-opt,-random-seed=42 -Wl,--plugin-opt,-randomize-function-list  -o $2 $1
+
+a=${1%.*}
+clang++ -flto -fuse-ld=gold -frandom-seed=42 -g -O0 -Wl,--plugin-opt,-random-seed=42 -Wl,--plugin-opt,-randomize-function-list  -o $a-secure $1
+
